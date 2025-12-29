be320bc4256b67035d0ac86e31dd3482761b9734
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-29 21:42:19 +0545

    updated main application to load env for application.properties

--- src/main/java/com/ridesharing/rideservice/RideServiceApplication.java
+++ src/main/java/com/ridesharing/rideservice/RideServiceApplication.java
@@ -1,5 +1,6 @@
 package com.ridesharing.rideservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RideServiceApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load(); // loads .env automatically
         SpringApplication.run(RideServiceApplication.class, args);
     }
 
