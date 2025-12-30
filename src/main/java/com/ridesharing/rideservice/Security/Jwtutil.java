88313e2c9aed6b2d333f350d240c58b97fbaa713
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-30 20:38:56 +0545

    updated application .properties

---
+++ src/main/java/com/ridesharing/rideservice/Security/Jwtutil.java
@@ -0,0 +1,30 @@
+package com.ridesharing.rideservice.Security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    private final Key key;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public Claims validateToken(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (JwtException ex) {
+            throw new RuntimeException("Invalid JWT token");
+        }
+    }
+}
