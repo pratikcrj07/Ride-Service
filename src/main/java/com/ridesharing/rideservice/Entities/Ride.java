fc1b1d3329d9991d9f56f888a8c327b1e33f2cdc
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-30 20:23:51 +0545

    ride entity created

---
+++ src/main/java/com/ridesharing/rideservice/Entities/Ride.java
@@ -0,0 +1,40 @@
+package com.ridesharing.rideservice.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "rides")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Ride {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // From JWT
+    private Long userId;
+
+    // Assigned later
+    private Long driverId;
+
+    @Enumerated(EnumType.STRING)
+    private RideStatus status;
+
+    private Double pickupLat;
+    private Double pickupLon;
+
+    private Double dropLat;
+    private Double dropLon;
+
+    private Double fare;
+
+    private LocalDateTime requestedAt;
+    private LocalDateTime completedAt;
+}
