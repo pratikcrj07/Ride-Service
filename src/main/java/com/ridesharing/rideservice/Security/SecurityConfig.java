13bebfae1623812deefa94e82bc5c93bc015b1a5
pratikcrj07 <pratikhumagaincr7@gmail.com>
2025-12-30 20:50:15 +0545

    security conig implemented for role base access

---
+++ src/main/java/com/ridesharing/rideservice/Security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.ridesharing.rideservice.Security;
+
+import com.ridesharing.rideservice.Security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.*;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthFilter JwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+
+                        // Ride request by USER
+                        .requestMatchers(HttpMethod.POST, "/api/rides/request")
+                        .hasRole("USER")
+
+                        // Driver APIs
+                        .requestMatchers("/api/driver/**")
+                        .hasRole("DRIVER")
+
+                        // Admin (future)
+                        .requestMatchers("/api/admin/**")
+                        .hasRole("ADMIN")
+
+                        // Everything else secured
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(JwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
