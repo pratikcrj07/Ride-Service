9d7f93410df9a3c77a1e8a8822a969cec0363b99
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-01-04 19:12:04 +0545

    ride repo updated

--- src/main/java/com/ridesharing/rideservice/Repository/RideRepository.java
+++ src/main/java/com/ridesharing/rideservice/Repository/RideRepository.java
@@ -5,10 +5,15 @@ import com.ridesharing.rideservice.Entities.RideStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RideRepository extends JpaRepository<Ride, Long> {
 
     List<Ride> findByUserIdOrderByRequestedAtDesc(Long userId);
+
+    Optional<Ride> findByIdAndStatus(Long id, RideStatus status);
+
+    Optional<Ride> findByIdAndDriverId(Long id, Long driverId);
 
     List<Ride> findByStatus(RideStatus status);
 }
