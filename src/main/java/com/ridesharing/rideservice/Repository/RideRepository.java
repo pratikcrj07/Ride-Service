16456414bde3a5ec99b91c6c3d41b1bc16be8daa
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-01-05 13:09:39 +0545

    updated repo for Db level security with lock mode

--- src/main/java/com/ridesharing/rideservice/Repository/RideRepository.java
+++ src/main/java/com/ridesharing/rideservice/Repository/RideRepository.java
@@ -2,7 +2,9 @@ package com.ridesharing.rideservice.Repository;
 
 import com.ridesharing.rideservice.Entities.Ride;
 import com.ridesharing.rideservice.Entities.RideStatus;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +13,9 @@ public interface RideRepository extends JpaRepository<Ride, Long> {
 
     List<Ride> findByUserIdOrderByRequestedAtDesc(Long userId);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Ride> findByIdAndStatus(Long id, RideStatus status);
+
 
     Optional<Ride> findByIdAndDriverId(Long id, Long driverId);
 
