c288db9368b1472b74ec551ff5bf5dfe3c9d3732
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-01-06 20:37:00 +0545

    added driver Location Controller

---
+++ src/main/java/com/ridesharing/rideservice/Controller/DriverLocationController.java
@@ -0,0 +1,11 @@
+package com.ridesharing.rideservice.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("api/driver/location")
+@RequiredArgsConstructor
+public class DriverLocationController {
+}
